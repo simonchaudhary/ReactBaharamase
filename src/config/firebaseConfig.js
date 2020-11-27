09f9d15bb004e9aa774f88d6c08273535516dc19
wearealldeveloper <cimon.chaudhary@gmail.com>
2020-11-27 13:31:29 +0545

    firebase added

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDQwL8NpHB7TvO13yzm9CY2mi-mzDHtiJg",
+    authDomain: "bahramasefirebase.firebaseapp.com",
+    databaseURL: "https://bahramasefirebase.firebaseio.com",
+    projectId: "bahramasefirebase",
+    storageBucket: "bahramasefirebase.appspot.com",
+    messagingSenderId: "930171359490",
+    appId: "1:930171359490:web:3d807136c314540edaf21b",
+    measurementId: "G-KDC8XHQJC5"
+};
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
